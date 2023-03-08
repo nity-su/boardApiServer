8add8e9905c6965748d33c2239c9dd28c69500b6
nity-su <7anlyn@gmail.com>
2023-03-08 16:32:17 +0900

    update index.js dialectModule

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,7 +4,7 @@ module.exports = {
   USER: process.env.DATABASE_USER,
   PASSWORD: process.env.DATABASE_PASS,
   DB: process.env.DATABASE_DB,
-  dialect: require("mysql2"),
+  dialect: require("mysql"),
   pool: {
     max: 5,
     min: 0,
--- app/models/index.js
+++ app/models/index.js
@@ -4,6 +4,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
+  dialectModule: require("mysql2"),
   operatorsAliases: false,
 
   pool: {
