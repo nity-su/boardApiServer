ea54faa020b31e7fde03e95a1acc0a144a48b9f8
nity-su <7anlyn@gmail.com>
2023-03-08 16:35:54 +0900

    update db.config,js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,7 +4,7 @@ module.exports = {
   USER: process.env.DATABASE_USER,
   PASSWORD: process.env.DATABASE_PASS,
   DB: process.env.DATABASE_DB,
-  dialect: require("mysql"),
+  dialect: "mysql",
   pool: {
     max: 5,
     min: 0,
